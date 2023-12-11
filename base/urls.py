fa0ee5d0765eccb478da1c3400562821c345da48
User <chernuhadmitry@gmail.com>
2023-12-11 21:33:41 +0300

    add models, templates, pagination

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from cities.views import CityDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cities.urls')),
+    path('cities/', include(('cities.urls', 'cities'))),
+
 
 ]
--- cities/views.py
+++ cities/views.py
@@ -1,5 +1,58 @@
-from django.shortcuts import render
+from django.core import paginator
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
+
+from cities.forms import CityForm
+from cities.models import City
 
 
-def cities(request):
-    return render(request, 'cities/index.html')
+def home(request, pk=None):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+    qs = City.objects.all()
+    form = CityForm()
+    lst = Paginator(qs, 2)
+    page_number = request.GET.get('page')
+    page_obj = lst.get_page(page_number)
+    context = {'page_obj': page_obj, 'form': form}
+    return render(request, 'cities/home.html', context)
+
+
+class CityDetailView(DetailView):
+    model = City
+    template_name = 'cities/detail.html'
+
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityUpdateView(UpdateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/update.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityDeleteView(DeleteView):
+    model = City
+    template_name = 'cities/delete.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityListView(ListView):
+    model = City
+    paginate_by = 5
+    template_name = 'cities/home.html'
+
+
+
+
