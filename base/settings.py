eb971d7b9d0033b5874b6c9be99e941cc3c847da
User <chernuhadmitry@gmail.com>
2023-12-12 10:28:33 +0300

    add alerts

--- base/settings.py
+++ base/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Local
     'cities.apps.CitiesConfig',
+    'trains.apps.TrainsConfig',
 ]
 
 MIDDLEWARE = [
--- cities/views.py
+++ cities/views.py
@@ -1,9 +1,10 @@
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.core import paginator
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
-
 from cities.forms import CityForm
 from cities.models import City
 
@@ -28,31 +29,33 @@ class CityDetailView(DetailView):
     template_name = 'cities/detail.html'
 
 
-class CityCreateView(CreateView):
+class CityCreateView(SuccessMessageMixin, CreateView):
     model = City
     form_class = CityForm
     template_name = 'cities/create.html'
     success_url = reverse_lazy('cities:home')
+    success_message = 'Город успешно создан'
 
 
-class CityUpdateView(UpdateView):
+class CityUpdateView(SuccessMessageMixin, UpdateView):
     model = City
     form_class = CityForm
     template_name = 'cities/update.html'
     success_url = reverse_lazy('cities:home')
+    success_message = 'Город успешно отредактирован'
 
 
 class CityDeleteView(DeleteView):
     model = City
-    template_name = 'cities/delete.html'
+    # template_name = 'cities/delete.html'
     success_url = reverse_lazy('cities:home')
+
+    def get(self, request, *args, **kwargs):
+        messages.success(request, 'Город успешно удален')
+        return self.post(request, *args, **kwargs)
 
 
 class CityListView(ListView):
     model = City
-    paginate_by = 5
+    paginate_by = 6
     template_name = 'cities/home.html'
-
-
-
-
