cf348b55279a1f1231d8c0aca6ec30a3bdd99c88
User <chernuhadmitry@gmail.com>
2023-12-14 21:47:11 +0300

    add find_routes logic

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from routes import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cities/', include(('cities.urls', 'cities'))),
     path('trains/', include(('trains.urls', 'trains'))),
+    path('', views.home, name='home'),
+    path('find_routes/', views.find_routes, name='find_routes'),
 ]
--- routes/views.py
+++ routes/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from cities.models import City
+from routes.forms import RouteForm
+from trains.models import Train
+
+
+def home(request):
+    form = RouteForm()
+    return render(request, 'routes/home.html', {'form': form})
+
+
+def find_routes(request):
+    if request.method == "POST":
+        form = RouteForm(request.POST)
+        if form.is_valid():
+            try:
+                context = get_routes(request, form)
+            except ValueError as e:
+                messages.error(request, e)
+                return render(request, 'routes/home.html', {'form': form})
+            return render(request, 'routes/home.html', context)
+        return render(request, 'routes/home.html', {'form': form})
+    else:
+        form = RouteForm()
+        messages.error(request, "Нет данных для поиска")
+        return render(request, 'routes/home.html', {'form': form})
+
+
+def add_route(request):
+    if request.method == "POST":
+        context = {}
+        data = request.POST
+        if data:
+            total_time = int(data['total_time'])
+            from_city_id = int(data['from_city'])
+            to_city_id = int(data['to_city'])
+            trains = data['trains'].split(',')
+            trains_lst = [int(t) for t in trains if t.isdigit()]
+            qs = Train.objects.filter(id__in=trains_lst).select_related(
+                'from_city', 'to_city')
+            cities = City.objects.filter(
+                id__in=[from_city_id, to_city_id]).in_bulk()
+            form = RouteModelForm(
+                initial={
+                    'from_city': cities[from_city_id],
+                    'to_city': cities[to_city_id],
+                    'travel_times': total_time,
+                    'trains': qs
+                         }
+            )
+            context['form'] = form
+        return render(request, 'routes/create.html', context)
+    else:
+        messages.error(request, "Невозможно сохранить несуществующий маршрут")
+        return redirect('/')
